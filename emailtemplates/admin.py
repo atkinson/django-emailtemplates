951e9ae7f3ac2dd2a3589bc735141bb37313cf3b
erm <erm@users.noreply.github.com>
2015-12-14 14:49:29 +1100

    updating admin

--- emailtemplates/admin.py
+++ emailtemplates/admin.py
@@ -6,7 +6,7 @@ class EmailTemplateAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'subject', 'visible_from_address')
     search_fields = ('name', 'slug', 'subject', 'from_address', 'body')
     fieldsets = (
-        (None, {'fields': ('name', 'slug', 'from_address')}),
+        (None, {'fields': ('name', 'slug', 'from_address', 'bcc')}),
         ('Email', {'fields': ('subject', 'body',)}),
         ('Advanced', {'classes': ('',), 'fields': ('base_template','txt_body')})
     )
